c15b66a6df4754d6c36bcc5ca3b51bd65bd48e7b
P.angelino <p.angelinodaniele@gmail.com>
2022-03-25 16:34:08 +0100

    jsnack7

---
+++ jsnack7/main.js
@@ -0,0 +1,16 @@
+function cuboDeiPrimiN_Numeri(n){
+    for(let i=1; i<=n; i++){
+        let cubo = i**3 ;
+        console.log(cubo);
+    }
+}
+
+
+
+
+let nNumeri = prompt("inserisci i primi n numeri di cui vuoi avere il cubo ")
+cuboDeiPrimiN_Numeri(nNumeri);
+
+
+
+
