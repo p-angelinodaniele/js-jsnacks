187535c1e742481781b47520e4837fc96543004a
P.angelino <p.angelinodaniele@gmail.com>
2022-03-25 13:23:44 +0100

    jsnack1

---
+++ jsnack1/main.js
@@ -0,0 +1,13 @@
+let numero1 = parseInt(prompt("Inserisci il numero 1"));
+let numero2 = parseInt(prompt("Inserisci il numero 2"));
+
+
+if(numero1 < numero2){
+    alert("il numero maggiore è " + numero2)
+}else if(numero1 > numero2){
+    alert("il numero maggiore è " + numero1)
+}else {
+    alert("I due numeri sono uguali")
+}
+
+
